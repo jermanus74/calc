9eb02410e549549df95a0f2e4b989906c10fcfb3
Zolo <jermanuspeter@gmail.com>
2023-06-18 14:34:04 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,34 @@
+const btn = document.querySelectorAll(".btn:not(#equal)");
+const input = document.querySelector(".input");
+const equal = document.querySelector("#equal");
+const operators = document.querySelectorAll(".operator");
+const del = document.querySelector(".del");
+
+function results(e) {
+  input.value += e.target.value;
+}
+
+btn.forEach((btn) => {
+  btn.addEventListener("click", results);
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", results);
+});
+
+calculate = function calc() {
+  const expression = input.value;
+  const result = eval(expression);
+  input.value = ` ${result}`;
+};
+
+function delete1() {
+  input.value = input.value.slice(0, -1);
+}
+function delete2() {
+  input.value = "";
+}
+del.addEventListener("click", delete1);
+del.addEventListener("dblclick", delete2);
+
+equal.addEventListener("click", calculate);
